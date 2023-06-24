864d86416fd239061342cd4077aad1b6cfea51bb
kariel-max <105429217+kariel-max@users.noreply.github.com>
2023-06-23 22:33:12 -0300

    Initial commit

---
+++ src/server/Sever.ts
@@ -0,0 +1,6 @@
+import {app} from "../server/app";
+
+
+app.listen(3333, ()=> {
+    console.log("conectou!!!!!!!");
+})
---
+++ src/server/app.ts
@@ -0,0 +1,11 @@
+import express,{Request,Response} from "express";
+
+const app = express();
+
+app.get('/', (req: Request, res: Response)=> {
+    return res.send("foi!!!!!!!")
+
+    
+})
+
+export {app}
