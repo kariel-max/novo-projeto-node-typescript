f430dcd9547a6e8563038a38b3e88ff55426bcc4
kariel-max <105429217+kariel-max@users.noreply.github.com>
2023-07-12 22:32:31 -0300

    foi

--- build/server/database/seed/ETablesNames.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ETablesNames = void 0;
-var ETablesNames;
-(function (ETablesNames) {
-    ETablesNames["cidade"] = "cidade";
-})(ETablesNames || (exports.ETablesNames = ETablesNames = {}));
