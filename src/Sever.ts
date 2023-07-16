b88072c994e7477313885dc6db4254807a8c7b88
kariel-max <105429217+kariel-max@users.noreply.github.com>
2023-07-16 13:53:01 -0300

    modificaçoes

--- src/Sever.ts
+++ src/Sever.ts
@@ -1,5 +1,23 @@
 import {app} from './server/app';
+import { Knex } from './server/database';
 
-app.listen(process.env.PORT, ()=> {
-    console.log("conectou!!!!!!!");
-})
+const startServer = () => {
+    app.listen(process.env.PORT || 3333, () => {
+      console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+    });
+  };
+  
+  if (process.env.IS_LOCALHOST !== 'true') {
+    console.log('Rodando migrations');
+  
+    Knex.migrate
+      .latest()
+      .then(() => {
+        Knex.seed.run()
+          .then(() => startServer())
+          .catch(console.log);
+      })
+      .catch(console.log);
+  } else {
+    startServer();
+  }
